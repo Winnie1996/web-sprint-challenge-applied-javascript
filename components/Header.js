db50110928eed111d9b592e1e5c414f65f42f5d0
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-08-07 08:16:18 -0700

    Finished Header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,37 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function coolHeader(){
+    //Creating my elements
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const h1 = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    //Adding Classes
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    //Text Contents
+    date.textContent = "July 7, 2020"
+    h1.textContent = 'Pacific NorthWest Times'
+    temp.textContent = 'It\s SummerTime Boys'
+
+    // date.textContent = 'MARCH 28, 2019';
+    // h1.textContent = 'Lambda Times';
+    // temp.textContent = '98 degrees';
+    
+
+    //Append
+    header.appendChild(date)
+    header.appendChild(h1)
+    header.appendChild(temp)
+
+    return header;
+
+}
+
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(coolHeader())
